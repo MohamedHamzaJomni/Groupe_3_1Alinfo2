9ee4edf3d99d90521f11c72fd1dd49d9b811a429
Hamza Jomni <jomnihamza@gmail.com>
2023-10-06 11:28:25 +0100

    hamza

---
+++ Groupe_3_1Alinfo2.git/src/hamza.java
@@ -0,0 +1,17 @@
+public class Hamza{
+    public void triTab(int [] t){
+        Arrays.sort(t)
+        System.out.println(t)
+    }
+}
+
+public class Hamza{
+    public void triTab(int [] t){
+        Arrays.sort(t);
+        System.out.println(t);
+    }
+    public String Majus(String ch){
+        ch.toUpperCase;
+        return ch;
+    }
+}
